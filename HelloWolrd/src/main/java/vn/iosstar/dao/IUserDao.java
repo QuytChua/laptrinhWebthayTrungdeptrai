3a4543d962c9f72e21bc6e94a753c3df869ebee4
Quốc Tuấn <22110261@student.hcmute.edu.vn>
2024-09-19 10:17:40 +0700

    Add files via upload

---
+++ HelloWolrd/src/main/java/vn/iosstar/dao/IUserDao.java
@@ -0,0 +1,16 @@
+package vn.iosstar.dao;
+import java.util.List;
+
+import vn.iosstar.models.UserModel;
+
+public interface IUserDao {
+
+	List<UserModel> findAll();
+	
+	UserModel findById(int id);
+	
+	void insert(UserModel user);
+	
+	UserModel findByUserName(String username);
+	
+}
